80f6fd024126de6777f6dfa3c9a0b54888afd79f
Terry Lequernaque <t.lequernaque@yahoo.com>
2023-01-17 01:51:14 -0500

    added confirm to js

--- Assets/Js/script.js
+++ Assets/Js/script.js
@@ -4,27 +4,44 @@
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
-var Numbers = "01234567890123456789";
-var UppercaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var LowercaseLetters = "abcdefghijklmnopqrstuvwxyz";
-var SpecialChar = "!@#$%^&*()_+[]{};:,.<>/?\|`~";
+var passwordLength = 8;
+var choiceArray = [];
+
+var NumberArray = "01234567890123456789";
+var UppercaseArray = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var LowercaseArray= "abcdefghijklmnopqrstuvwxyz";
+var SpecialCharArray = "!@#$%^&*()_+[]{};:,.<>/?\|`~";
 
 
 
 function generatePassword(){
   var passwordLength = (prompt ("Between 8 -128 characters, How many Characters would you like your password to contain"));
 
-  if(passwordLength < 8){
-    alert("Password must be at least 8 or more characters long!");
-  } else if(passwordLength > 128){
-    alert("Password must not be more than 128 characters long!");
-  } else{ 
-    var userChoice = confirm ("Would you like to include special characters");
-    var userChoice = confirm ("Would you like to include uppercase characters");
-    var userChoice = confirm ("Would you like to include lowercase characters");
+  if(passwordLength < 8 || passwordLength > 128){
+    alert("Password must be between least 8 and 128 characters long!");
+    return false
+  } 
+  
+  if (confirm ("Would you like to include number characters")){
+    choiceArray = choiceArray.concat(NumberArray);
+  }  
+  
+  if (confirm ("Would you like to include special characters")){
+    choiceArray = choiceArray.concat(SpecialCharArray);
   }
+    
+  if (confirm ("Would you like to include uppercase characters")){
+    choiceArray = choiceArray.concat(UppercaseArray);
+  }  
+    
+  if (confirm ("Would you like to include lowercase characters")){
+    choiceArray = choiceArray.concat(LowercaseArray);
+  }
+  
 
-}
+  }
+    var random = Math.floor(Math.random() * 10) + 1;
+
 
 
 
