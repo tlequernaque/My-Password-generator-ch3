059cceb8fd46cb7541b8638a955c4bfc0ba3c3ad
Terry Lequernaque <t.lequernaque@yahoo.com>
2023-01-18 00:37:06 -0500

    added final changes to js

--- Assets/Js/script.js
+++ Assets/Js/script.js
@@ -4,22 +4,20 @@
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
-var passwordLength = 8;
 var choiceArray = [];
 
 var NumberArray = [ "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9" ]
 var UppercaseArray = [ "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z" ];
 var LowercaseArray= [ "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z" ];
-var SpecialCharArray = [ "~", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")","-", "=", "+", ",", "<", ">", ".", "?", ";", ":", "[", "]", "{", "}", ];
+var SpecialCharArray = [ "~", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")","-", "=", "+", ",", "<", ">", ".", "?", ";", ":", "[", "]", "{", "}", ] ;
 
 
 
 function generatePassword(){
-  var passwordLength = (prompt ("Between 8 -128 characters, How many Characters would you like your password to contain"));
-
-  if(passwordLength < 8 || passwordLength > 128){
+  var passwordLength = parseInt(prompt ("Between 8 -128 characters, How many Characters would you like your password to contain"));
+  if(passwordLength < 8 || passwordLength > 128 || isNaN(passwordLength)){
     alert("Password must be between least 8 and 128 characters long!");
-    return false
+    return 
   } 
   
   if (confirm ("Would you like to include number characters")){
@@ -37,20 +35,23 @@ function generatePassword(){
   if (confirm ("Would you like to include lowercase characters")){
     choiceArray = choiceArray.concat(LowercaseArray);
   }
-  
+
+  var password = "";
+  for(var i = 0; i < passwordLength; i++){
+    var randomIndex = Math.floor(Math.random() * choiceArray.length);
+    var randomCharacter = choiceArray[randomIndex];
+    password += randomCharacter;
+  } return password;
 
 }
-    var random = Math.floor(Math.random() * 10) + 1;
-
-
 
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
+if (password){
+  passwordText.value = password;};
 
 }
 
